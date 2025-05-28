0661a5e00abfcba3cc5ac2faa87f65d8065f86c0
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2025-05-28 14:41:51 +0700

    is admin default value changes

--- server/database/migrations/2025_05_27_181807_adding_is_admin_column_in_user.php
+++ server/database/migrations/2025_05_27_181807_adding_is_admin_column_in_user.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->smallInteger('is_admin');
+            $table->smallInteger('is_admin')->default(0);
         });
     }
 
