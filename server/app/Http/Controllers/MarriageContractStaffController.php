14d6f2dda0baaa66da37e29a408d425edd62c666
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2025-06-02 13:44:02 +0700

    controller and model resource update

--- server/app/Http/Controllers/MarriageContractStaffController.php
+++ server/app/Http/Controllers/MarriageContractStaffController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MarriageContractStaff;
 use Illuminate\Http\Request;
 
-class MarriageContactStaff extends Controller
+class MarriageContractStaffController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MarriageContractStaff $marriageContractStaff)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MarriageContractStaff $marriageContractStaff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MarriageContractStaff $marriageContractStaff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MarriageContractStaff $marriageContractStaff)
+    {
+        //
+    }
 }
--- server/app/Http/Controllers/ReceptionStaffController.php
+++ server/app/Http/Controllers/ReceptionStaffController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ReceptionStaff;
 use Illuminate\Http\Request;
 
 class ReceptionStaffController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReceptionStaff $receptionStaff)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ReceptionStaff $receptionStaff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ReceptionStaff $receptionStaff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReceptionStaff $receptionStaff)
+    {
+        //
+    }
 }
