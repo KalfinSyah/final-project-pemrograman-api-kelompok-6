8314d0f202ba32bb76745fb23324182fdb7d414b
Kalfin Syah Kilau Mayya <kalfinsyah.km@gmail.com>
2025-06-08 23:18:04 +0700

    update companey profile : add services functionality (100%)

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 import { ref } from 'vue';
-const selectedService = ref('');
+const selectedService = ref('PS');
 
 </script>
 
@@ -28,23 +28,44 @@ const selectedService = ref('');
     <h2>Our Services</h2>
     
     <div id="service-list">
-      <div @click="selectedService = 'PS'">Photoshoot</div>
-      <div @click="selectedService = 'WO'">Wedding Organizer</div>
-      <div @click="selectedService = 'EO'">Event Organizer</div>
+      <div @click="selectedService = 'PS'"  :class="{ 'active-service' : selectedService === 'PS' }">Photoshoot</div>
+      <div @click="selectedService = 'WO'"  :class="{ 'active-service' : selectedService === 'WO' }">Wedding Organizer</div>
+      <div @click="selectedService = 'EO'"  :class="{ 'active-service' : selectedService === 'EO' }">Event Organizer</div>
     </div>
 
     <div id="service-details">
-      <div v-if="selectedService === 'PS'">
-        <h3>Photoshoot</h3>
-        <p>We offer professional photoshoot services for various occasions.</p>
+      <div id="service-details-container" v-if="selectedService === 'WO'">
+        <img id="service-img" src="../assets/services/WO.jpg" alt="Service Image">
+        <div id="service-desc">
+          <h3>Wedding Organizer</h3>
+          <div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, ullam ab. Minima cumque esse sunt nisi nulla, repudiandae consequatur veniam, ex tempora saepe optio totam ut maxime eligendi dolor impedit?
+              Beatae possimus alias autem odio ipsam nulla! Minus rem iure fugiat facilis corrupti cumque sunt sint error, tempore quis alias vero deserunt excepturi aliquam quibusdam eos, animi nobis totam dignissimos.
+              Architecto maiores vel ullam cumque commodi unde voluptate a blanditiis, similique quod, nam praesentium incidunt. Repellat cum natus veritatis! Aspernatur fuga porro dolorum corrupti culpa quisquam quod est blanditiis quae?
+              Odit nihil, ipsa totam sequi molestiae dignissimos quasi, voluptas sapiente nemo blanditiis quas quos dolores iure ullam consequatur autem, in similique. Esse, temporibus doloremque perferendis repellendus officiis alias nemo provident.
+              Aut animi ea fugit at officia minus, fugiat ipsam, repellendus inventore reprehenderit aspernatur. Dolore sed repudiandae labore. Culpa, dolorum delectus, praesentium, quod assumenda minus ipsam quisquam odio earum ab eligendi!
+            </p>
+          </div>
+        </div>
       </div>
-      <div v-else-if="selectedService === 'WO'">
-        <h3>Wedding Organizer</h3>
-        <p>Let us help you plan your perfect wedding day.</p>
+      <div id="service-details-container" v-else-if="selectedService === 'EO'">
+        <img id="service-img" src="../assets/services/EO.jpg" alt="Service Image">
+        <div id="service-desc">
+          <h3>Event Organizer</h3>
+          <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, ad asperiores! Earum, explicabo numquam quas itaque a ut voluptas modi harum illo similique exercitationem reiciendis! Ipsum ab blanditiis quam est!</p>
+          </div>
+        </div>
       </div>
-      <div v-else-if="selectedService === 'EO'">
-        <h3>Event Organizer</h3>
-        <p>We organize events that leave lasting impressions.</p>
+      <div id="service-details-container" v-else="selectedService === 'PS'">
+        <img id="service-img" src="../assets/services/PS.jpg" alt="Service Image">
+        <div id="service-desc">
+          <h3>Photoshoot</h3>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus iure, laboriosam maiores quo sed voluptate tempora ratione distinctio velit magni rerum officiis sapiente aut omnis eaque libero. Alias, delectus sunt!</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -143,6 +164,52 @@ const selectedService = ref('');
 
 #service-details {
   margin-top: 50px;
+}
+
+#service-details-container {
+  display: grid;
+  width: 1000px;
+  height: 400px;
+  background-color: whitesmoke;
+  margin: auto;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas: 
+  'img desc desc'
+  'img desc desc';
+  border-radius: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+}
+
+#service-img {
+  grid-area: img;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+#service-desc {
+  grid-area: desc;
+  height: 100%;
+  overflow: hidden;
+}
+
+#service-desc h3 {
+  width: fit-content;
+  margin-top: 40px;
+  margin-left: 40px;
+}
+
+#service-desc div {
+  margin: 20px 40px 20px 40px;
+  text-align: justify;
+  height: 70%;
+  overflow: auto;
+}
+
+.active-service {
+  transform: translateY(-2px);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 </style>
