4cc5a34d9836409592ac185bdcb555c38d9c208c
UWONG990 <dwijoutomorahinoputro@gmail.com>
2025-06-18 03:11:27 +0700

    Cashflow Graph

--- client/src/components/CashflowCard.vue
+++ client/src/components/CashflowCard.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="bg-white rounded-2xl shadow-lg p-8 flex flex-col justify-between min-h-[240px] text-gray-800">
-    <h2 class="text-lg font-bold text-[#2F3367] mb-2">Cashflow</h2>
-    <div>
-      <div class="flex justify-between text-sm mb-1">
+    <h2 class="text-lg font-bold text-[#2F3367] mb-8">Cashflow</h2>
+    <div class="flex-1 flex flex-col justify-between">
+      <div class="flex justify-between text-sm mb-4">
         <span>Masuk</span>
         <span class="font-semibold text-right">IDR {{ masuk.toLocaleString() }}</span>
       </div>
-      <div class="w-full h-3 bg-gray-200 rounded-full mb-2">
+      <div class="w-full h-3 bg-gray-200 rounded-full mb-8">
         <div
           class="h-3 bg-[#2F3367] rounded-full"
           :style="{ width: masukBar + '%' }"
         ></div>
       </div>
-      <div class="flex justify-between text-sm mb-1">
+      <div class="flex justify-between text-sm mb-4">
         <span>Keluar</span>
         <span class="font-semibold text-right">-IDR {{ keluar.toLocaleString() }}</span>
       </div>
-      <div class="w-full h-3 bg-gray-200 rounded-full">
+      <div class="w-full h-3 bg-gray-200 rounded-full mb-8">
         <div
           class="h-3 bg-[#2F3367] rounded-full"
           :style="{ width: keluarBar + '%' }"
